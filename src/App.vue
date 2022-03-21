fdf975aa41dfd6e3c6a6b443209564ac7f93dc22
hananhasis <hanan2497@gmail.com>
2022-03-21 11:39:29 +0200

    another stage

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App222"/>
-    <div>this is a git tutorial</div>
+    <div>this is a git tutorial 2</div>
   </div>
 </template>
 
