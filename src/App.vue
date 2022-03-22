9e66e86374e07e263076e48521a3a6fd434d66af
hananhasis <hanan2497@gmail.com>
2022-03-22 10:01:46 +0200

    added new lines2

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App222"/> -->
     <h1>Hello World</h1>
+    <div>this is a git tutorial 1</div>
     <div>this is a git tutorial 2</div>
-    <div>this is a git tutorial 2</div>
-    <div>this is a git tutorial 2</div>
-    <div>this is a git tutorial 2</div>
+    <div>this is a git tutorial 3</div>
+    <div>this is a git tutorial 4</div>
   </div>
 </template>
 
